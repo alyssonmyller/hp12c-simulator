f98c29cf68848940d8c187386e36f03755559cb6
Alysson Myller <alyssonmyller@gmail.com>
2026-06-03 15:43:18 -0300

    nome da versao

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.alyssonmyller.calculus.hp12c"
         minSdk        = libs.versions.minSdk.get().toInt()
         targetSdk     = libs.versions.targetSdk.get().toInt()
-        versionCode   = 1
-        versionName   = "0.0.1"
+        versionCode   = 2
+        versionName   = "0.0.1-beta"
     }
 
     buildFeatures {
